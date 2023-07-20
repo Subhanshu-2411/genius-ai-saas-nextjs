a7fdcb85c86e42d1f30f0f98380a750d88a06fc3
Subhanshu-2411 <subhashu20135@iiitd.ac.in>
2023-07-20 12:20:49 +0530

    Added Root CSS in Global CSS

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
       <Button
-          variant="danger"
+          variant="destructive"
+          size="lg"
       >
           Click Me
       </Button>
